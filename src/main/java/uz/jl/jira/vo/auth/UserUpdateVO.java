26edfaba06d7ee72d09d8d7b17d1fc69042d15c5
Muhammadqodir <yusupovforwin@gmail.com>
2022-06-15 19:02:55 +0500

    commit

---
+++ src/main/java/uz/jl/jira/vo/auth/UserUpdateVO.java
@@ -0,0 +1,13 @@
+package uz.jl.jira.vo.auth;
+
+import uz.jl.jira.vo.BaseVO;
+import uz.jl.jira.vo.GenericVO;
+
+/**
+ * @author "Elmurodov Javohir"
+ * @since 14/06/22/15:41 (Tuesday)
+ * jira/IntelliJ IDEA
+ */
+public class UserUpdateVO extends GenericVO implements BaseVO {
+
+}
