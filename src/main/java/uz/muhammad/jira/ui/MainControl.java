7ecd110d68ae6f38d1a2b315eb81339085d50667
yusupov06 <yusupovforwin@gmail.com>
2022-06-16 12:01:31 +0500

    added MainControl.java

---
+++ src/main/java/uz/muhammad/jira/ui/MainControl.java
@@ -0,0 +1,14 @@
+package uz.muhammad.jira.ui;
+
+/**
+ * @author Team <Developers>
+ * @project TrelloBY
+ * @since 16/06/22   11:54   (Thursday)
+ */
+public class MainControl {
+    public static void control(){
+
+
+
+    }
+}
